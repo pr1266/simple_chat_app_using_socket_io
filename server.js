7ea0a844486265bfb9bf10b580462640c60d8a2c
pr1266 <pourya_1999@yahoo.com>
2020-07-20 12:43:42 -0700

    'statics'

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 
-const PORT = 3000 || process.env.PORT
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when a client connects
+io.on('connection', socket =>{
+    console.log('new WS connected');
+});
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
